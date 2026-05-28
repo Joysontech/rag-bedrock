75b3fdabd588afdf0a1f34b14ea3dd0856c18675
Joyson Fernandes <jf@joysontech.com>
2026-05-28 20:47:41 +0100

    feat(lambda): wire Bedrock Prompt Management
    - IAM: add bedrock:GetPrompt scoped to prompt JRKRWLNLD2
    - Env: PROMPT_ARN = arn:aws:bedrock:eu-west-2:...:prompt/JRKRWLNLD2:1
    - To roll out a new prompt version, bump the :1 suffix and apply

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -109,6 +109,16 @@ data "aws_iam_policy_document" "lambda" {
   }
 
   statement {
+    sid    = "BedrockPromptGet"
+    effect = "Allow"
+    actions = ["bedrock:GetPrompt"]
+    resources = [
+      "arn:aws:bedrock:eu-west-2:061051257340:prompt/JRKRWLNLD2",
+      "arn:aws:bedrock:eu-west-2:061051257340:prompt/JRKRWLNLD2:*",
+    ]
+  }
+
+  statement {
     sid    = "SecretsRead"
     effect = "Allow"
     actions = [
@@ -204,6 +214,8 @@ locals {
     GENERATION_MODEL_ID = "eu.anthropic.claude-haiku-4-5-20251001-v1:0"
     GUARDRAIL_ID        = "hy14n4r45o6f"
     GUARDRAIL_VERSION   = "1"
+    # Versioned prompt ARN: update suffix to roll out a new prompt version
+    PROMPT_ARN          = "arn:aws:bedrock:eu-west-2:061051257340:prompt/JRKRWLNLD2:1"
     LOG_LEVEL           = "INFO"
   }
 }
